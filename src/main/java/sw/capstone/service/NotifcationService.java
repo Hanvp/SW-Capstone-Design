b56de69598d6e574d6d671014df2ea0e68910ab2
Hanvp <nwactris@naver.com>
2023-10-12 14:20:40 +0900

    :sparkles: Feature/24 rabbitMQ 연결 설정 완료

--- src/main/java/sw/capstone/service/NotifcationService.java
+++ src/main/java/sw/capstone/service/NotifcationService.java
@@ -1,12 +1,20 @@
 package sw.capstone.service;
 
+import sw.capstone.web.dto.requestDto.EmailRequestDto;
 import sw.capstone.web.dto.requestDto.FcmRequestDto;
 import sw.capstone.web.dto.requestDto.SmsRequestDto;
+import sw.capstone.web.dto.responseDto.EmailResponseDto;
 import sw.capstone.web.dto.responseDto.FcmResponseDto;
 import sw.capstone.web.dto.responseDto.SmsResponseDto;
 
 public interface NotifcationService {
     FcmResponseDto.FcmResultDto sendFcmWorker(FcmRequestDto.request request);
 
-    SmsResponseDto.SmsResultDto sendSmsWorker(SmsRequestDto.request request);
+    SmsResponseDto.SmsResultDto sendSmsRedisWorker(SmsRequestDto.request request);
+
+    SmsResponseDto.SmsResultDto sendSmsMqWorker(SmsRequestDto.request request);
+
+    EmailResponseDto.EmailResultDto sendEmailRedisWorker(EmailRequestDto.request request);
+
+    EmailResponseDto.EmailResultDto sendEmailMqWorker(EmailRequestDto.request request);
 }
