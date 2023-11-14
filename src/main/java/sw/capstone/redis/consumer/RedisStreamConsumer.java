f807fa981a258732617517bbae9eb4f74fc6542f
Hanvp <nwactris@naver.com>
2023-11-14 14:34:42 +0900

    :sparkles: Email-redis tail latency, jitter 측정으로 변경

--- src/main/java/sw/capstone/redis/consumer/RedisStreamConsumer.java
+++ src/main/java/sw/capstone/redis/consumer/RedisStreamConsumer.java
@@ -20,6 +20,8 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import sw.capstone.converter.EmailConverter;
@@ -31,10 +33,9 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.time.ZoneId;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Slf4j
@@ -58,10 +59,8 @@ public class RedisStreamConsumer implements StreamListener<String, MapRecord<Str
     // 위에 구현한 Redis Streamd에 필요한 기본 Command를 구현한 Component
     private final RedisOperator redisOperator;
 
-    List<CheckRecordPer1000> recordDto = new ArrayList<>();
-
-    private Boolean start = true;
-    AtomicLong count= new AtomicLong(0);
+    List<Long> result = new ArrayList<>();
+    AtomicInteger count= new AtomicInteger(0);
 
     private String setContext(String randomNum) {
         Context context = new Context();
@@ -73,61 +72,19 @@ public class RedisStreamConsumer implements StreamListener<String, MapRecord<Str
     public void onMessage(MapRecord<String, Object, Object> record) {
         // 처리할 로직 구현
 
-        if(count.getAndIncrement() % 10 == 0)
-            log.info(LocalDateTime.now() + ": " + count.get());
+        Long now = System.currentTimeMillis();
+        String sendTime = (String) record.getValue().get("claimTime");
 
-        log.info(record.toString());
-        String data = (String) record.getValue().get("info");
-        log.info(data);
+//        log.info("Now: "+now+", Send Time: "+sendTime);
+
+        Long differ = now - Long.parseLong(sendTime);
+
+        result.add(differ);
+        count.getAndIncrement();
 
         // 이후, ack stream
         this.redisOperator.ackStream("email", record);
 
-//        // 처리할 로직 구현
-//        log.info(record.toString());
-//
-//        String data = (String) record.getValue().get("info");
-//
-//        Optional<EmailRedisStream> value = null;
-//
-//        try {
-//            ObjectMapper objectMapper = new ObjectMapper();
-//            value = Optional.of(objectMapper.readValue(data, EmailRedisStream.class));
-//        } catch (JsonProcessingException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        if (value.get() == null){
-//            log.error("정보가 안담겨옴");
-//        }
-//        else {
-//            if (start){
-//                recordDto.add(new CheckRecordPer1000(1L, LocalDateTime.now()));
-//                start = false;
-//            }
-//
-//            if (count.getAndIncrement() % 10000 == 0) {
-//                recordDto.add(new CheckRecordPer1000(count.get()-1, LocalDateTime.now()));
-//            }
-//
-//            //처리할 로직 구현
-//            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-//
-//            try {
-//                MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
-//                mimeMessageHelper.setTo(value.get().getTargetEmail());
-//                mimeMessageHelper.setSubject("sw-capstone 인증 이메일 테스트");
-//                mimeMessageHelper.setText(setContext(value.get().getRandomNum()), true);
-//                javaMailSender.send(mimeMessage);
-//
-//                log.info(EmailConverter.toEmailResultDto(value.get().getTargetEmail(), value.get().getRandomNum(), "success").toString());
-//            } catch (MessagingException e) {
-//                log.info(EmailConverter.toEmailResultDto(value.get().getTargetEmail(), value.get().getRandomNum(), "fail").toString());
-//            }
-//
-//            // 이후, ack stream
-//            this.redisOperator.ackStream("email", record);
-//        }
     }
 
     @Override
@@ -167,7 +124,117 @@ public class RedisStreamConsumer implements StreamListener<String, MapRecord<Str
         this.listenerContainer.start();
     }
 
-    public void getLog() {
-        recordDto.stream().forEach(record -> log.info(record.getCount() + ": "+ record.getLocalDateTime()));
+    public void getLog(int size) {
+        Collections.sort(result);
+//        result.sort((a, b) -> a.compareTo(b));
+
+        int p50 = (int)(size * 0.5);
+        int p90 = (int)(size * 0.9);
+        int p99 = (int)(size * 0.99);
+        int p999 = (int)(size * 0.999);
+        int p9999 = (int)(size * 0.9999);
+
+        log.info("p50, p90, p99, p999, p999: "+p50+", "+p90+", "+p99+", "+p999+", "+p9999);
+
+        Long sum = 0L;
+
+        //---- tail latency ----//
+        for (int i = 0; i < p50; i++) {
+            sum += result.get(i);
+        }
+        log.info("p50 Average Tail Latency: "+ sum /(p50* 1.0));
+        log.info("p50 Max Tail Latency: "+ result.get(p50-1));
+
+        for (int i = p50+1; i < p90; i++) {
+            sum += result.get(i);
+        }
+        log.info("p90 Average Tail Latency: "+ sum /(p90* 1.0));
+        log.info("p90 Max Tail Latency: "+ result.get(p90-1));
+
+
+        for (int i = p90+1; i < p99; i++) {
+            sum += result.get(i);
+        }
+        log.info("p99 Average Tail Latency: "+ sum /(p99* 1.0));
+        log.info("p99 Max Tail Latency: "+ result.get(p99-1));
+
+
+        for (int i = p99+1; i < p999; i++) {
+            sum += result.get(i);
+        }
+        log.info("p999 Average Tail Latency: "+ sum /(p999* 1.0));
+        log.info("p999 Max Tail Latency: "+ result.get(p999-1));
+
+
+        for (int i = p999+1; i < p9999; i++) {
+            sum += result.get(i);
+        }
+        log.info("p9999 Average Tail Latency: "+ sum /(p9999* 1.0));
+        log.info("p9999 Max Tail Latency: "+ result.get(p9999-1));
+
+
+        for (int i = p9999+1; i < size; i++) {
+            sum += result.get(i);
+        }
+
+        //---- Jitter ----//
+        List<Double> jitter = new ArrayList<>();
+
+        double average = sum / (size * 1.0);
+        log.info("Average: "+ average);
+
+        for (int i = 0; i < size; i++) {
+            jitter.add(result.get(i) - average);
+        }
+
+        List<Long> jitterGraph = new ArrayList<>(Collections.nCopies(20, 0L));
+
+        Collections.sort(jitter);
+//        Double firstValue = jitter.get(0);
+//        Double lastValue = jitter.get(jitter.size() - 1);
+//        Double differ = lastValue-firstValue;
+//        double section = differ / 100.0;
+//
+//        log.info("differ: "+differ+", 1% section: "+section);
+
+        int index = 0;
+
+        for (int i = 0; i < 20; i++) {
+//            double start = firstValue + section * i;
+//            double end = start + section;
+            double start = i - 7;
+            double end = start + 1;
+
+            while (jitter.get(index) < end) {
+                if(jitter.get(index) >= start) {
+                    jitterGraph.set(i, jitterGraph.get(i) + 1L);
+                    index++;
+                }
+            }
+
+            log.info(start+"~"+end+"구간: "+jitterGraph.get(i)/(size*1.0)*100+"%");
+        }
+    }
+
+    public void addData(String ip) {
+
+        log.info("추가 전 size: " + result.size());
+        
+        String uri = "http://" + ip + ":8080/data";
+
+        WebClient webClient = WebClient.create();
+        List<Long> returnValue = webClient.get()
+                .uri(uri)
+                .retrieve()
+                .bodyToFlux(Long.class)
+                .collectList()
+                .block();
+
+        result.addAll(returnValue);
+        log.info("추가 후 size: " + result.size());
+    }
+
+    public List<Long> returnData() {
+        return result;
     }
 }
--- src/main/java/sw/capstone/web/controller/RootRestController.java
+++ src/main/java/sw/capstone/web/controller/RootRestController.java
@@ -3,14 +3,13 @@ package sw.capstone.web.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sw.capstone.redis.consumer.RedisStreamConsumer;
 import sw.capstone.service.EmailService;
 import sw.capstone.web.dto.requestDto.EmailRequestDto;
 import sw.capstone.web.dto.responseDto.EmailResponseDto;
+
+import java.util.List;
 
 @RestController
 @Validated
@@ -31,8 +30,18 @@ public class RootRestController {
         return emailService.sendEmail(request);
     }
 
-    @GetMapping("/log")
-    public void getCount() {
-        redisStreamConsumer.getLog();
+    @GetMapping("/log/{size}")
+    public void getCount(@PathVariable int size) {
+        redisStreamConsumer.getLog(size);
+    }
+
+    @PostMapping("/data/{ip}")
+    public void addData(@PathVariable String ip) {
+        redisStreamConsumer.addData(ip);
+    }
+
+    @GetMapping("/data")
+    public List<Long> returnData() {
+        return redisStreamConsumer.returnData();
     }
 }
