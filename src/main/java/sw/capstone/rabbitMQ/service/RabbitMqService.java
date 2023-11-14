3ffe146cec2acad734692d890d9a0172499c46fc
Hanvp <nwactris@naver.com>
2023-11-14 15:49:07 +0900

    :sparkles: Email-rabbitMQ tail latency와 jitter 측정으로 변경

--- src/main/java/sw/capstone/rabbitMQ/service/RabbitMqService.java
+++ src/main/java/sw/capstone/rabbitMQ/service/RabbitMqService.java
@@ -8,6 +8,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import sw.capstone.converter.EmailConverter;
@@ -18,7 +19,10 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
 
@@ -34,9 +38,8 @@ public class RabbitMqService {
     @Value("${spring.mail.auth-code-expiration-millis}")
     private Long authCodeExpirationMillis;
 
-    Boolean start = true;
-    AtomicLong count = new AtomicLong(1);
-    List<CheckRecordPer1000> recordDto = new ArrayList<>();
+    List<Long> result = new ArrayList<>();
+    AtomicInteger count= new AtomicInteger(0);
 
 
     private String setContext(String randomNum) {
@@ -46,45 +49,130 @@ public class RabbitMqService {
     }
 
     @RabbitListener(queues = "${spring.rabbitmq.queue.email}")
-    public void receiveMessage(byte[] content) {
+    public void receiveMessage(Map<String, Object> content) {
 
-        if(count.get() % 10 == 0)
-            log.info(LocalDateTime.now() + ": " + count.getAndIncrement());
+        Long now = System.currentTimeMillis();
+        String sendTime = content.get("claimTime").toString();
 
-//        if (rabbitMQDto == null){
-//            log.error("정보가 안담겨옴");
-//        }
-//        else {
-//            if (start){
-//                recordDto.add(new CheckRecordPer1000(1L, LocalDateTime.now()));
-////                log.info("1st record consume time: " + LocalDateTime.now());
-//                start = false;
-//            }
-//
-//            if (count.getAndIncrement() % 1000 == 0) {
-//                recordDto.add(new CheckRecordPer1000(count.get()-1, LocalDateTime.now()));
-//            }
-//
-//            log.info(rabbitMQDto.getTargetEmail(), rabbitMQDto.getRandomNum());
-//
-//            //처리할 로직 구현
-//            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-//
-//            try {
-//                MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
-//                mimeMessageHelper.setTo(rabbitMQDto.getTargetEmail());
-//                mimeMessageHelper.setSubject("sw-capstone 인증 이메일 테스트");
-//                mimeMessageHelper.setText(setContext(rabbitMQDto.getRandomNum()), true);
-//                javaMailSender.send(mimeMessage);
-//
-////                log.info(EmailConverter.toEmailResultDto(rabbitMQDto.getTargetEmail(), rabbitMQDto.getRandomNum(), "success").toString());
-//            } catch (MessagingException e) {
-////                log.info(EmailConverter.toEmailResultDto(rabbitMQDto.getTargetEmail(), rabbitMQDto.getRandomNum(), "fail").toString());
-//            }
-//        }
+//        log.info("Now: "+now+", Send Time: "+sendTime);
+
+        Long differ = now - Long.parseLong(sendTime);
+
+        result.add(differ);
+        count.getAndIncrement();
     }
 
-    public void getLog() {
-        recordDto.stream().forEach(record -> log.info(record.getCount() + ": " + record.getLocalDateTime()));
+    public void getLog(int size) {
+        Collections.sort(result);
+//        result.sort((a, b) -> a.compareTo(b));
+
+        int p50 = (int)(size * 0.5);
+        int p90 = (int)(size * 0.9);
+        int p99 = (int)(size * 0.99);
+        int p999 = (int)(size * 0.999);
+        int p9999 = (int)(size * 0.9999);
+
+        log.info("p50, p90, p99, p999, p999: "+p50+", "+p90+", "+p99+", "+p999+", "+p9999);
+
+        Long sum = 0L;
+
+        //---- tail latency ----//
+        for (int i = 0; i < p50; i++) {
+            sum += result.get(i);
+        }
+        log.info("p50 Average Tail Latency: "+ sum /(p50* 1.0));
+        log.info("p50 Max Tail Latency: "+ result.get(p50-1));
+
+        for (int i = p50+1; i < p90; i++) {
+            sum += result.get(i);
+        }
+        log.info("p90 Average Tail Latency: "+ sum /(p90* 1.0));
+        log.info("p90 Max Tail Latency: "+ result.get(p90-1));
+
+
+        for (int i = p90+1; i < p99; i++) {
+            sum += result.get(i);
+        }
+        log.info("p99 Average Tail Latency: "+ sum /(p99* 1.0));
+        log.info("p99 Max Tail Latency: "+ result.get(p99-1));
+
+
+        for (int i = p99+1; i < p999; i++) {
+            sum += result.get(i);
+        }
+        log.info("p999 Average Tail Latency: "+ sum /(p999* 1.0));
+        log.info("p999 Max Tail Latency: "+ result.get(p999-1));
+
+
+        for (int i = p999+1; i < p9999; i++) {
+            sum += result.get(i);
+        }
+        log.info("p9999 Average Tail Latency: "+ sum /(p9999* 1.0));
+        log.info("p9999 Max Tail Latency: "+ result.get(p9999-1));
+
+
+        for (int i = p9999+1; i < size; i++) {
+            sum += result.get(i);
+        }
+
+        //---- Jitter ----//
+        List<Double> jitter = new ArrayList<>();
+
+        double average = sum / (size * 1.0);
+        log.info("Average: "+ average);
+
+        for (int i = 0; i < size; i++) {
+            jitter.add(result.get(i) - average);
+        }
+
+        List<Long> jitterGraph = new ArrayList<>(Collections.nCopies(20, 0L));
+
+        Collections.sort(jitter);
+//        Double firstValue = jitter.get(0);
+//        Double lastValue = jitter.get(jitter.size() - 1);
+//        Double differ = lastValue-firstValue;
+//        double section = differ / 100.0;
+//
+//        log.info("differ: "+differ+", 1% section: "+section);
+
+        int index = 0;
+
+        for (int i = 0; i < 20; i++) {
+//            double start = firstValue + section * i;
+//            double end = start + section;
+            double start = i - 7;
+            double end = start + 1;
+
+            while (jitter.get(index) < end) {
+                if(jitter.get(index) >= start) {
+                    jitterGraph.set(i, jitterGraph.get(i) + 1L);
+                    index++;
+                }
+            }
+
+            log.info(start+"~"+end+"구간: "+jitterGraph.get(i)/(size*1.0)*100+"%");
+        }
+    }
+
+    public void addData(String ip) {
+
+        log.info("추가 전 size: " + result.size());
+
+        String uri = "http://" + ip + ":8080/data";
+
+        WebClient webClient = WebClient.create();
+        List<Long> returnValue = webClient.get()
+                .uri(uri)
+                .retrieve()
+                .bodyToFlux(Long.class)
+                .collectList()
+                .block();
+
+        result.addAll(returnValue);
+        log.info("추가 후 size: " + result.size());
+    }
+
+    public List<Long> returnData() {
+        return result;
     }
 }
